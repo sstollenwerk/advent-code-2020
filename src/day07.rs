ecd196a539232be7529190e530eccd580a08c525
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-12-31 23:19:35 +1300

    Do day07 part2

--- src/day07.rs
+++ src/day07.rs
@@ -3,6 +3,8 @@ type Num = u32;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
+use itertools::Itertools;
+
 use crate::utility::map_values;
 
 type Node = String;
@@ -56,23 +58,41 @@ fn parents(b: &Bags) -> Bags {
     res
 }
 
-fn posses(start: &Node, b: Bags) -> HashSet<Node> {
+fn posses(start: &Node, b: Bags) -> Vec<HashSet<Node>> {
     let groups: HashMap<Node, Vec<Node>> =
         map_values(b, |r| r.iter().map(|x| x.1.clone()).collect::<Vec<_>>());
 
     let mut currents: HashSet<String> = HashSet::from([start.clone()]);
-    let mut total = currents.clone();
+    let mut totals = Vec::new();
     while !currents.is_empty() {
         currents = currents
             .into_iter()
             .flat_map(|k| groups.get(&k).unwrap_or(&Vec::new()).clone())
             .collect();
-        currents = &currents - &total;
-        total = &total | &currents;
+        totals.push(currents.clone());
     }
 
-    total.remove(start);
-    total
+    totals
+}
+
+fn required(start: &Node, b: &Bags) -> Num {
+    let mut xs = posses(start, b.clone());
+    xs.reverse();
+
+    xs.push(HashSet::from([start.clone()]));
+
+    let mut cache: HashMap<Node, Num> = HashMap::new();
+
+    for val in xs.into_iter().flatten().unique() {
+        let p = b[&val].clone();
+
+        let total: Num = 1 + p.iter().map(|(amt, b)| amt * cache[b]).sum::<Num>();
+
+        cache.insert(val, total);
+    }
+
+    cache[start] - 1
+    // -1 because want ot ignore gold bag itself
 }
 
 fn parse(base: &str) -> Bags {
@@ -83,11 +103,15 @@ pub fn part1(base: &str) -> usize {
     let data = parse(base);
 
     let inv = parents(&data);
-    posses(&"shiny gold".to_string(), inv).len()
+    posses(&"shiny gold".to_string(), inv)
+        .into_iter()
+        .flatten()
+        .collect::<HashSet<_>>()
+        .len()
 }
 
 pub fn part2(base: &str) -> Num {
     let data = parse(base);
 
-    todo!();
+    required(&"shiny gold".to_string(), &data)
 }
